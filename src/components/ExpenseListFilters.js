473ac78af65b77c61dff502f4a1245b44691b5f0
Imre Bartis <imre.bartis@siili.com>
2018-01-23 14:52:57 +0200

    dropdown for picking sortBy

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,16 +1,28 @@
 import React from 'react';
-import { connect } from 'react-redux'; // connecting the component to the store
-import { setTextFilter } from '../actions/filters';
+import { connect } from 'react-redux';
+import { setTextFilter, sortByDate, sortByAmount } from '../actions/filters';
 
 const ExpenseListFilters = (props) => (
   <div>
-    <input type="text" value={props.filters.text} onChange={(e) => {
-      props.dispatch(setTextFilter(e.target.value));
-    }} />
+    <input
+      type="text"
+      value={props.filters.text}
+      onChange={(e) => {
+        props.dispatch(setTextFilter(e.target.value));
+      }}
+    />
+    <select
+      value={props.filters.sortBy}
+      onChange={(e) => { 
+          e.target.value === 'date' ? props.dispatch(sortByDate()) : props.dispatch(sortByAmount()) 
+        }}
+    >
+      <option value="date">Date</option>
+      <option value="amount">Amount</option>
+    </select>
   </div>
 );
 
-// filters will have access to props.text
 const mapStateToProps = (state) => {
   return {
     filters: state.filters
