9ad7a9cf87bd8f6c05dd49ce690da3dc6c230484
Imre Bartis <imre.bartis@siili.com>
2018-02-13 14:36:36 +0200

    setting up Enzyme

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import ReactShallowRenderer from 'react-test-renderer/shallow';
+import { shallow } from 'enzyme';
+// import ReactShallowRenderer from 'react-test-renderer/shallow';
+import toJSON from 'enzyme-to-json';
 import Header from '../../components/Header';
 
 test('should render Header correctly', () => {
-  const renderer = new ReactShallowRenderer();
-  renderer.render(<Header />);
-  expect(renderer.getRenderOutput()).toMatchSnapshot();
+  const wrapper = shallow(<Header />);
+  expect(wrapper.find('h1').length).toBe(1);
+  expect(wrapper.find('h1').text()).toBe('Expensify');
+  expect(wrapper).toMatchSnapshot();
+  
+  // const renderer = new ReactShallowRenderer();
+  // renderer.render(<Header />);
+  // expect(renderer.getRenderOutput()).toMatchSnapshot();
 });
