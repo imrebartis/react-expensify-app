5778fd0be1854851bcb52633652ea785102d7c2d
Imre Bartis <imre.bartis@siili.com>
2018-02-14 11:06:33 +0200

    testing the EditExpensePage

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -5,7 +5,7 @@ import { addExpense } from '../actions/expenses';
 
 export class AddExpensePage extends React.Component {
   onSubmit = (expense) => {
-    this.props.onSubmit(expense);
+    this.props.addExpense(expense);
     this.props.history.push('/');
   };
   render() {
@@ -14,14 +14,14 @@ export class AddExpensePage extends React.Component {
         <h1>Add Expense</h1>
         <ExpenseForm
           onSubmit={this.onSubmit}
-          />
-        </div>
+        />
+      </div>
     );
   }
 }
 
 const mapDispatchToProps = (dispatch) => ({
-  onSubmit: (expense) => dispatch(addExpense(expense))
+  addExpense: (expense) => dispatch(addExpense(expense))
 });
 
 export default connect(undefined, mapDispatchToProps)(AddExpensePage);
